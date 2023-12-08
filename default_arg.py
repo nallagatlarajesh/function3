f68d8708c3dfe8a66995a10ea11f697f2578a11d
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-08 18:01:06 +0530

    Create default_arg.py

---
+++ default_arg.py
@@ -0,0 +1,8 @@
+
+
+def sayhello(name,age,country="india"):
+    print("hello",name)
+    print("age",age)
+    print("iam from",country)
+sayhello(age=30,name="rajesh")
+sayhello("emily",23,"america")
