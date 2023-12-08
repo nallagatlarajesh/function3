68b472b113da81f9c09ccd490ec44d9459550678
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-08 16:24:47 +0530

    Create 2argumentts.py

---
+++ 2argumentts.py
@@ -0,0 +1,13 @@
+def sayHello(name="user"):
+    print("hai",name)
+    print("good morning")
+def add(fn,sn):
+    result=fn+sn
+    print("result",result)
+
+add(25,36)
+sayHello('rajesh')
+print("some block of code....")
+sayHello("sravs")
+sayHello()
+add(100,300)
